db9e09488aa093df7f514a2f35de7265bb9bb209
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2024-09-08 20:57:10 +0530

    use pg8000 to work with lambda

--- lambda_scripts/lambda_function.py
+++ lambda_scripts/lambda_function.py
@@ -1,15 +1,36 @@
 import io
 import os
+import pg8000
 import re
 import requests
 import zipfile
 
-from sqlalchemy import create_engine
+from contextlib import contextmanager
 
-POSTGRES_DB = os.environ['DATABASE']
-POSTGRES_USER = os.environ['USER']
-POSTGRES_PASSWORD = os.environ['PASSWORD']
-POSTGRES_HOST = os.environ['HOSTNAME']
+
+DB_HOST = os.environ['HOSTNAME']
+DB_NAME = os.environ['DATABASE']
+DB_USER = os.environ['USER']
+DB_PASSWORD = os.environ['PASSWORD']
+
+@contextmanager
+def get_db_connection():
+    try:
+        conn = pg8000.connect(
+            host=DB_HOST,
+            port=5432,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        yield conn
+        conn.commit()
+    except Exception as e:
+        print(f"Database connection error: {e}")
+        raise
+    finally:
+        conn.close()
+
 
 def download_matches(matches_url):
     response = requests.get(matches_url)
@@ -22,23 +43,26 @@ def download_matches(matches_url):
             else:
                 zip_ref.extract(info, '/tmp/matches/')
 
+def load_files_to_db(folder_path, table_name, conn):
+    try:
+        for file_name in os.listdir(folder_path):
+            with open(os.path.join(folder_path, file_name), 'r') as file:
+                data = file.read().strip()  # Adjust data formatting if necessary
+                sql_query = f"INSERT INTO {table_name} VALUES {data}"
+                conn.run(sql_query)
+                print(f"Data from {file_name} loaded into {table_name}")
+    except Exception as e:
+        print(f"Error loading data from {folder_path}: {e}")
+
 def load_data_to_db():
-    POSTGRES_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
+    with get_db_connection() as conn:
+        # Load match info data
+        load_files_to_db('/tmp/matches_info/', 'raw.infos', conn)
+        
+        # Load match deliveries data
+        load_files_to_db('/tmp/matches/', 'raw.deliveries', conn)
 
-    engine = create_engine(POSTGRES_URL)
-    conn = engine.connect()
-
-    for file in os.listdir('/tmp/matches_info/'):
-        with open(f'/tmp/matches_info/{file}', 'r') as f:
-            data = f.read()
-            conn.execute(f"INSERT INTO raw.infos VALUES {data}")
-    
-    for file in os.listdir('/tmp/matches/'):
-        with open(f'/tmp/matches/{file}', 'r') as f:
-            data = f.read()
-            conn.execute(f"INSERT INTO raw.deliveries VALUES {data}")
-
-def lambda_handler(event, context):
+def lambda_handler(event):
     url = event['url']
 
     download_matches(url)
