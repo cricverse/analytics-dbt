120eae4244fa3d0bd92b7d93827440b5b1ddbfea
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2024-09-20 15:01:08 +0530

    change in team summary

--- dbt/models/mart/mart_team_summary.sql
+++ dbt/models/mart/mart_team_summary.sql
@@ -6,21 +6,16 @@ teams AS (
 )
 
 SELECT 
-    DISTINCT team_name,
-    matches_played,
-    series_played,
-    matches_won
-FROM (
-    SELECT 
-        team_name,
-        match_type,
-        ARRAY_AGG(DISTINCT match_id) AS matches_played,
-        ARRAY_AGG(DISTINCT series_name) AS series_played,
-        CASE WHEN winner = team_name THEN ARRAY_AGG(DISTINCT match_id) ELSE NULL END AS matches_won
-    FROM teams
-    LEFT JOIN match_details
-    ON teams.team_name = match_details.team1
-    OR teams.team_name = match_details.team2
+    team_name,
+    match_type,
+    match_id,
+    winner,
+    series_name,
+    match_num,
+    match_stage,
+    season
 
-    GROUP BY team_name, match_type, winner
-) AS team_summary
+FROM teams
+LEFT JOIN match_details
+ON teams.team_name = match_details.team1
+OR teams.team_name = match_details.team2
