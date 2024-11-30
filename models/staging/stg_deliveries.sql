f5e0d4d0dd6d3694af1bbe1f0a8eca5a9f8b435a
Prateek Bhardwaj <prateekb1912@gmail.com>
2024-11-30 22:12:14 +0530

    add new deliveries model + add staging

---
+++ models/staging/stg_deliveries.sql
@@ -0,0 +1,55 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": ["match_id", "inning", "over", "ball"]
+}) }}
+
+WITH delivery_data AS (
+    SELECT
+        match_id,
+        jsonb_array_elements(deliveries) AS delivery
+    FROM {{ ref('raw_matches') }}
+),
+
+innings_data AS (
+    SELECT
+        match_id,
+        ROW_NUMBER() OVER() AS inning,
+        delivery->>'team' AS batting_team,
+        jsonb_array_elements(delivery->'overs') AS overs
+    FROM delivery_data
+),
+
+overs_data AS (
+    SELECT 
+        match_id,
+        inning,
+        batting_team,
+        overs->>'over' AS over,
+        jsonb_array_elements(overs->'deliveries') AS deliveries
+    FROM innings_data
+)
+
+SELECT 
+    match_id,
+    inning,
+    batting_team,
+    over,
+    row_number() over() AS ball,
+    deliveries->>'batter' AS batter,
+    deliveries->>'bowler' AS bowler,
+    (deliveries->'runs'->>'total')::int AS runs_total,
+    (deliveries->'runs'->>'batter')::int AS runs_batter,
+    (deliveries->'runs'->>'extras')::int AS runs_extras,
+    deliveries->>'non_striker' AS non_striker,
+
+    CASE WHEN deliveries->'wickets'->0 IS NOT NULL THEN TRUE ELSE FALSE END AS is_wicket,
+    deliveries->'wickets'->0->>'kind' AS wicket_type,
+    deliveries->'wickets'->0->>'player_out' AS player_dismissed,
+    deliveries->'wickets'->0->>'fielders' AS fielders,
+
+    COALESCE(deliveries->'extras'->>'wides', '0')::int AS wide_runs,
+    COALESCE(deliveries->'extras'->>'no_balls', '0')::int AS noball_runs,
+    COALESCE(deliveries->'extras'->>'byes', '0')::int AS bye_runs,
+    COALESCE(deliveries->'extras'->>'legbyes', '0')::int AS legbye_runs
+
+FROM overs_data
