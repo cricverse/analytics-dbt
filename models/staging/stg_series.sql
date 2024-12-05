eb433082b5d2dd2d18f2559b4e8cc0e85d3c24fb
Prateek Bhardwaj <prateekb1912@gmail.com>
2024-12-05 15:07:46 +0530

    update series

--- models/staging/stg_series.sql
+++ models/staging/stg_series.sql
@@ -1,11 +1,16 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": ["series_id"]
+}) }}
+
 WITH matches AS (
     SELECT * FROM {{ ref('stg_matches') }}
 )
 
 SELECT
-    MD5(CONCAT(series_name, season, format)) AS series_id,
+    MD5(CONCAT(series_name, season)) AS series_id,
     series_name,
     season,
-    format
+    ARRAY_AGG(DISTINCT format ORDER BY format) AS formats
 FROM matches
-GROUP BY series_name, season, format
+GROUP BY series_name, season
