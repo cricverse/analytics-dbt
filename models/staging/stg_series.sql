05c2d58ac556a12ff2bd36e5704b44c1c2ee311f
Prateek Bhardwaj <prateekb1912@gmail.com>
2024-12-05 15:13:55 +0530

    add teams

--- models/staging/stg_series.sql
+++ models/staging/stg_series.sql
@@ -11,6 +11,7 @@ SELECT
     MD5(CONCAT(series_name, season)) AS series_id,
     series_name,
     season,
-    ARRAY_AGG(DISTINCT format ORDER BY format) AS formats
+    ARRAY_AGG(DISTINCT format ORDER BY format) AS formats,
+    ARRAY_AGG(DISTINCT team1 ORDER BY team1) || ARRAY_AGG(DISTINCT team2 ORDER BY team2) AS teams
 FROM matches
 GROUP BY series_name, season
